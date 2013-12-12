b6865ef542a9fe8c6f06b33ef8138d927217722d
Daniel Kocsan <dani@kocsan.hu>
2013-12-12 21:54:45 +0100

    remove grunt-mocha from Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,7 +50,6 @@ module.exports = function (grunt) {
         }
     );
 
-    grunt.loadNpmTasks('grunt-mocha');
     grunt.loadNpmTasks('grunt-jslint');
     grunt.loadNpmTasks('grunt-contrib-jasmine');
     grunt.loadNpmTasks('grunt-contrib-watch');
